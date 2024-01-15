4c4fb35603729fe06c12c8b25efebdfd255e1713
Lydia Günther <lydia.guenther@student.uni-luebeck.de>
2024-01-15 16:12:37 +0100

    Test Three with the Case of Ones, Twos etc.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,21 +1,56 @@
-import javax.sql.rowset.Predicate;
 import java.util.Arrays;
 
 public class Game {
     public int score(int[] roll, String category) {
         int score = 0;
-        if (category.equals("yatzy")){
-            if(Arrays.stream(roll).allMatch(i -> i == roll[0])){
-                score = 50;
-            }
+        switch (category) {
+            case "yatzy":
+                if (Arrays.stream(roll).allMatch(i -> i == roll[0])) {
+                    score = 50;
+                }
 
-        }
-        else {
-            for (int dice : roll) {
-                score += dice;
+                break;
+            case "ones":
+                score = (int) Arrays.stream(roll).filter(i -> i == 1).count();
+
+                break;
+            case "twos": {
+                int quantityOfFours = (int) Arrays.stream(roll).filter(i -> i == 2).count();
+                score = 2 * quantityOfFours;
+
+                break;
             }
+            case "threes": {
+                int quantityOfFours = (int) Arrays.stream(roll).filter(i -> i == 3).count();
+                score = 3 * quantityOfFours;
+
+                break;
+            }
+            case "fours": {
+                int quantityOfFours = (int) Arrays.stream(roll).filter(i -> i == 4).count();
+                score = 4 * quantityOfFours;
+
+                break;
+            }
+            case "fives": {
+                int quantityOfFours = (int) Arrays.stream(roll).filter(i -> i == 5).count();
+                score = 5 * quantityOfFours;
+
+                break;
+            }
+            default:
+                score = sumOfAllDice(roll, score);
+                break;
         }
 
         return score;
     }
+
+    private int sumOfAllDice(int[] roll, int score) {
+        for (int dice : roll) {
+            score += dice;
+        }
+        return score;
+    }
+
 }
--- src/test/java/YatzyGameTest.java
+++ src/test/java/YatzyGameTest.java
@@ -16,16 +16,32 @@ public class YatzyGameTest {
         int[] roll = {1,1,3,3,6};
         assertThat(game.score(roll, "chance"), is(14));
     }
-
     @Test
     public void rollAYatzyWithOnes(){
         int[] roll = {1,1,1,1,1};
         assertThat(game.score(roll, "yatzy"), is(50));
     }
-
     @Test
     public void rollAYatzyWithOnesAndOneTwoExpectZero(){
         int[] roll = {1,2,1,1,1};
         assertThat(game.score(roll, "yatzy"), is(0));
+    }
+
+    @Test
+    public void rollDicesWithTwoFours(){
+        int[] roll = {1,1,2,4,4};
+        assertThat(game.score(roll,"fours") ,is(8));
+    }
+
+    @Test
+    public void rollDicesWithTwoTwos(){
+        int[] roll = {2,3,2,5,1};
+        assertThat(game.score(roll,"twos") ,is(4));
+    }
+
+    @Test
+    public void rollDicesWithNoOneExpectedZero(){
+        int[] roll = {3,3,3,4,5};
+        assertThat(game.score(roll,"ones") ,is(0));
     }
 }
