e98441cba916850ac1aecc8acc97872ed659df2d
Lydia Günther <lydia.guenther@student.uni-luebeck.de>
2024-01-15 15:45:16 +0100

    methode setUp in YatzyGameTest to avoid code duplicates

--- src/test/java/YatzyGameTest.java
+++ src/test/java/YatzyGameTest.java
@@ -1,28 +1,30 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.in;
 import static org.hamcrest.Matchers.is;
 
 public class YatzyGameTest {
+    Game game;
+    @Before
+    public void setUp(){
+        game = new Game();
+    }
 
     @Test
     public void rollAChanceWithScoreFourteen(){
-        Game game = new Game();
         int[] roll = {1,1,3,3,6};
         assertThat(game.score(roll, "chance"), is(14));
     }
 
     @Test
     public void rollAYatzyWithOnes(){
-        Game game = new Game();
         int[] roll = {1,1,1,1,1};
         assertThat(game.score(roll, "yatzy"), is(50));
     }
 
     @Test
     public void rollAYatzyWithOnesAndOneTwoExpectZero(){
-        Game game = new Game();
         int[] roll = {1,2,1,1,1};
         assertThat(game.score(roll, "yatzy"), is(0));
     }
